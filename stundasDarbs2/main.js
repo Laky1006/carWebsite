a221200ae919d8579017ebb80f31fd85f5f89862
Anna Hr <ah00238@rvt.lv>
2024-10-02 13:08:29 +0300

    gyatt

--- stundasDarbs2/main.js
+++ stundasDarbs2/main.js
@@ -1,20 +1,25 @@
 // Get the modal
-var modal = document.getElementById("Eppy-Cat-Mod");
+var modal = document.getElementById("eppy-cat-mod");
+var modalOrange = document.getElementById("Orange-Mod");
+var modalWizard = document.getElementById("Wizard-Mod");
 
 // Get the button that opens the modal
 var btn = document.getElementById("eepy-cat-btn");
+var btnOrange = document.getElementById("orange-btn");
+var btnWizard = document.getElementById("wizard-btn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+function close_btn() {
+    modal.style.display = "none";
+    modalOrange.style.display = "none";
+    modalWizard.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -24,48 +29,26 @@ window.onclick = function(event) {
   }
 }
 
-// // Get the modal
-// var modalOrange = document.getElementById("Orange-Mod");
+// When the user clicks on the button, open the modal
+btnOrange.onclick = function() {
+    modalOrange.style.display = "block";
+}
 
-// // Get the button that opens the modal
-// var btnOrange = document.getElementById("orange-btn");
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalOrange) {
+    modalOrange.style.display = "none";
+  }
+}
 
-// // When the user clicks on the button, open the modal
-// btnOrange.onclick = function() {
-//     modalOrange.style.display = "block";
-// }
+// When the user clicks on the button, open the modal
+btnWizard.onclick = function() {
+    modalWizard.style.display = "block";
+}
 
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modalOrange.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modalOrange) {
-//     modalOrange.style.display = "none";
-//   }
-// }
-
-// // Get the modal
-// var modalWizard = document.getElementById("Wizard-Mod");
-
-// // Get the button that opens the modal
-// var btnWizard = document.getElementById("wizard-btn");
-
-// // When the user clicks on the button, open the modal
-// btnWizard.onclick = function() {
-//     modalWizard.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modalWizard.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modalWizard) {
-//     modalWizard.style.display = "none";
-//   }
-// }
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalWizard) {
+    modalWizard.style.display = "none";
+  }
+}
